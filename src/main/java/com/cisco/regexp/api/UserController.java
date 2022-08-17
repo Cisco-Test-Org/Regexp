658272c6c2e2638c3f72d8324c40c76e645ed466
sovisrushain <sovisrushain@gmail.com>
2022-08-18 01:43:01 +0530

    basic resources

--- src/main/java/com/cisco/regexp/api/UserController.java
+++ src/main/java/com/cisco/regexp/api/UserController.java
@@ -1,14 +1,36 @@
 package com.cisco.regexp.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cisco.regexp.model.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1")
 public class UserController {
     // get all users
+    @GetMapping
+    public List<User> getUserList() {
+        return null;
+    }
     // get a user
+    @GetMapping
+    public User getUser(@PathVariable String id) {
+        return null;
+    }
     // save user
+    @PostMapping
+    public void saveUser(@RequestBody User user) {
+
+    }
     // update user
+    @PutMapping
+    public void updateUser(@RequestBody User user, @PathVariable String id) {
+
+    }
     // delete user
+    @DeleteMapping
+    public void deleteUser(@PathVariable String id) {
+
+    }
 }
