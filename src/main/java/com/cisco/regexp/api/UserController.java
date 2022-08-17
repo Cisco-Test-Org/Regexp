d67939ea55871924be78509d50b82a18a56efb8e
sovisrushain <sovisrushain@gmail.com>
2022-08-18 01:33:26 +0530

    initialize api layer

---
+++ src/main/java/com/cisco/regexp/api/UserController.java
@@ -0,0 +1,14 @@
+package com.cisco.regexp.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class UserController {
+    // get all users
+    // get a user
+    // save user
+    // update user
+    // delete user
+}
